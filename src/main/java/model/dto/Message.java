fb49c7cdac898ddea4df6ca5f3a928277ddfde2a
Adrian Lucena <adrianlucenavaro@gmail.com>
2023-09-26 09:46:06 +0200

    adrianlucenaa

--- src/main/java/model/dto/Message.java
+++ src/main/java/model/dto/Message.java
@@ -1,4 +1,50 @@
 package model.dto;
 
 public class Message {
+        private User usuario;
+        private String contenido;
+        private Date fecha;
+
+    public Message() {
+    }
+
+    public Message(String message, String contenido, Date fecha) {
+        Message = message;
+        Contenido = contenido;
+        Fecha = fecha;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
+
+    public String getContenido() {
+        return Contenido;
+    }
+
+    public void setContenido(String contenido) {
+        Contenido = contenido;
+    }
+
+    public Date getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        Fecha = fecha;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Message{" +
+                "usuario=" + usuario +
+                ", contenido='" + contenido + '\'' +
+                ", fecha=" + fecha +
+                '}';
+    }
+
 }
--- src/main/java/model/dto/User.java
+++ src/main/java/model/dto/User.java
@@ -1,4 +1,41 @@
 package model.dto;
 
 public class User {
+
+    private String name;
+    private Date fecha;
+
+    public User() {
+    }
+
+
+    public User(String name, Date fecha) {
+        Name = name;
+        Fecha = fecha;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Date getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        Fecha = fecha;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "User{" +
+                "Name='" + Name + '\'' +
+                ", Fecha=" + Fecha +
+                '}';
+    }
+
 }
